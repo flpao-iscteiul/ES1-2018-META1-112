29a42b37e15f350aa6632c83ac41656b8f3a5e7e
João Duque <jglvd@iscte-iul.pt>
2018-10-16 14:20:27 +0100

    Test commit

--- BomDiaA/src/app/Main.java
+++ BomDiaA/src/app/Main.java
@@ -3,7 +3,7 @@ package app;
 import app.Aplication;
 import window.Intro_Window;
 
-public class Main {
+public class Main { //Test
 	public static void main(String [] args){
 		Aplication apl =new Aplication();
 	}
