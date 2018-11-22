acc3d4847230ea54a7dd91f937d19a567bca10dd
Francisco Palma <flpao@iscte-iul.pt>
2018-11-22 16:34:47 +0000

    TestInit2_window_1.3

--- BomDiaA/src/window/App_Window.java
+++ BomDiaA/src/window/App_Window.java
@@ -7,6 +7,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.FileNotFoundException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -23,6 +24,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import email.Email;
+import facebook.Connect;
 
 public class App_Window {
 	private JFrame frame;
@@ -139,6 +141,114 @@ public class App_Window {
 				frameEmail.add(bodyText);
 				frameEmail.add(sendButton);
 				
+			}
+		});
+	}
+	
+	public void makeFacebookPost() {
+		subMenuItemFb.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame frameFacebook = new JFrame("New Facebook Post");
+				frameFacebook.setSize(1000, 200);
+				JPanel panelHolder = new JPanel();    
+				panelHolder.setLayout(new GridLayout(1,2));
+				JPanel panelHolder2 = new JPanel();    
+				panelHolder2.setLayout(new GridLayout(1,3));
+				JPanel panelHolder3 = new JPanel();    
+				panelHolder3.setLayout(new GridLayout(1,4));
+				JPanel panelHolder4 = new JPanel();    
+				panelHolder4.setLayout(new GridLayout(1,3));
+				frameFacebook.setLayout(new GridLayout(4, 1));
+				frameFacebook.setLocationRelativeTo(frame);
+				frameFacebook.setVisible(true);
+
+				JLabel postMessage = new JLabel("Insert Message ");
+				JLabel postLink = new JLabel("Insert Message & Link");
+				JLabel postImage = new JLabel("Insert Image Name & Directory & Message");
+
+				JTextField simpleMessage= new JTextField();
+				JTextField linkMessage = new JTextField();
+				JTextField link = new JTextField();
+				JTextField imageName= new JTextField();
+				JTextField directory = new JTextField();
+				JTextField imageMessage = new JTextField();
+
+				JButton sendPost = new JButton("SendMessage");
+				JButton sendPost2 = new JButton("SendLink");
+				JButton sendPost3 = new JButton("SendImage");
+				sendPost.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						Connect c = new Connect();
+						c.establishConnection();
+
+						if(!simpleMessage.getText().equals(null)) {
+
+							c.publishMessageStatus(simpleMessage.getText());
+							simpleMessage.setText(null);
+						}
+					}
+				});
+
+				sendPost2.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						Connect c = new Connect();
+						c.establishConnection();
+
+						if(!link.getText().equals(null)) {
+							System.out.println("Trying to publish");
+							c.publishLinkStatus(linkMessage.getText(), link.getText());
+							System.out.println("Done");
+							linkMessage.setText(null);
+							link.setText(null);
+						}
+
+					}
+				});
+
+				sendPost3.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						Connect c = new Connect();
+						c.establishConnection();
+
+						if(!imageName.getText().equals(null) && !directory.getText().equals(null)) {
+
+							try {
+								c.publishImageStatus(imageName.getText(), directory.getText(), imageMessage.getText());
+							} catch (FileNotFoundException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+							imageName.setText(null);
+							directory.setText(null);
+							imageMessage.setText(null);
+
+						}
+					}});
+
+				panelHolder.add(postMessage);
+				panelHolder.add(simpleMessage);
+				panelHolder2.add(postLink);
+				panelHolder2.add(linkMessage);
+				panelHolder2.add(link);
+				panelHolder3.add(postImage);
+				panelHolder3.add(imageName);
+				panelHolder3.add(directory);
+				panelHolder3.add(imageMessage);
+				panelHolder4.add(sendPost);
+				panelHolder4.add(sendPost2);
+				panelHolder4.add(sendPost3);
+				frameFacebook.add(panelHolder);
+				frameFacebook.add(panelHolder2);
+				frameFacebook.add(panelHolder3);
+				frameFacebook.add(panelHolder4);
 			}
 		});
 	}
