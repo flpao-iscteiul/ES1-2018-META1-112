71ba8ae27a197fcdc93a463f033bd0a44c3d8fb8
Francisco Palma <flpao@iscte-iul.pt>
2018-11-22 16:19:27 +0000

    TestInit_app_1.4

--- BomDiaA/src/app/Aplication.java
+++ BomDiaA/src/app/Aplication.java
@@ -1,5 +1,27 @@
 package app;
 
-public class Aplication {
+import window.App_Window;
+import window.Intro_Window;
 
+
+public class Aplication extends Thread{
+
+	
+	public Aplication() {
+		Intro_Window iw = new Intro_Window();
+		App_Window app = new App_Window();
+		iw.open();
+		
+		try {
+			sleep(2000);
+			iw.close();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		app.open();
+	}
+	
+	
+	
 }
